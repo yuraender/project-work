8f37a516d928536f91a12e783968552ec494a909
YuraEnder <yuraender@yandex.ru>
2023-05-24 08:17:55 +0300

    Add graph image for clarity

--- ProjectWork/Forms/Tasks/TaskSevenForm.Designer.cs
+++ ProjectWork/Forms/Tasks/TaskSevenForm.Designer.cs
@@ -28,8 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -40,12 +40,14 @@
             this.MinColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.MaxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ExpectedValueColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(270, 270);
+            this.button1.Location = new System.Drawing.Point(270, 434);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(142, 51);
             this.button1.TabIndex = 6;
@@ -57,7 +59,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
-            this.label1.Location = new System.Drawing.Point(12, 277);
+            this.label1.Location = new System.Drawing.Point(12, 441);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(96, 29);
             this.label1.TabIndex = 1;
@@ -67,7 +69,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
-            this.label2.Location = new System.Drawing.Point(12, 306);
+            this.label2.Location = new System.Drawing.Point(12, 470);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(96, 29);
             this.label2.TabIndex = 2;
@@ -77,7 +79,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
-            this.label3.Location = new System.Drawing.Point(12, 335);
+            this.label3.Location = new System.Drawing.Point(12, 499);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(96, 29);
             this.label3.TabIndex = 3;
@@ -87,7 +89,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
-            this.label4.Location = new System.Drawing.Point(12, 364);
+            this.label4.Location = new System.Drawing.Point(12, 528);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(96, 29);
             this.label4.TabIndex = 4;
@@ -97,7 +99,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
-            this.label5.Location = new System.Drawing.Point(12, 414);
+            this.label5.Location = new System.Drawing.Point(12, 578);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(236, 29);
             this.label5.TabIndex = 5;
@@ -115,15 +117,15 @@
             this.MinColumn,
             this.MaxColumn,
             this.ExpectedValueColumn});
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle6;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 169);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToFirstHeader;
             this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.None;
@@ -142,17 +144,28 @@
             // 
             // ExpectedValueColumn
             // 
-            dataGridViewCellStyle1.BackColor = System.Drawing.Color.LightGray;
-            this.ExpectedValueColumn.DefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle5.BackColor = System.Drawing.Color.LightGray;
+            this.ExpectedValueColumn.DefaultCellStyle = dataGridViewCellStyle5;
             this.ExpectedValueColumn.HeaderText = "Мат. ожидание";
             this.ExpectedValueColumn.Name = "ExpectedValueColumn";
             this.ExpectedValueColumn.ReadOnly = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ProjectWork.Properties.Resources.graph;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 151);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
             // 
             // TaskSevenForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(424, 461);
+            this.ClientSize = new System.Drawing.Size(424, 621);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -167,6 +180,7 @@
             this.Text = "Задача сетевого планирования и управления";
             this.Load += new System.EventHandler(this.TaskSevenForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -183,5 +197,6 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn MinColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn MaxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn ExpectedValueColumn;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
